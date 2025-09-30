f02d48e09d39f7fd370981f2d2c8d4ec7ff75b24
equinox-1092 <sdiaz1234di@gmail.com>
2025-09-30 00:24:17 -0500

    feat(sign-in): add components of the loing in sign in

--- src/app/authentication/pages/sign-in/sign-in.component.ts
+++ src/app/authentication/pages/sign-in/sign-in.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sign-in',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule, 
+    ReactiveFormsModule,
+    RouterModule,
+    MatButtonModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule,
+    MatIconModule,
+    TranslateModule
+  ],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  styleUrls: ['./sign-in.component.css']
 })
 export class SignInComponent {
+  loginForm: FormGroup;
+  showRegister = false;
+  hidePassword = true;
+  
+  constructor(
+    private fb: FormBuilder,
+    private router: Router
+  ) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      rememberMe: [false]
+    });
+  }
 
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted', this.loginForm.value);
+    }
+  }
+
+  toggleForm() {
+    this.router.navigate(['/sign-up']);
+  }
 }
