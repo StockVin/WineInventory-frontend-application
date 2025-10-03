0adc923adddcdc8c953d55b8f52655c52c866837
Giovany7x <u202311601@upc.edu.pe>
2025-10-03 13:11:30 -0500

    feat(orders):  update environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api/v1',
-  baseServerUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'http://localhost:3000',
+  baseServerUrl: 'http://localhost:3000/api/v1',
 };
