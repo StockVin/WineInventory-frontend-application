a314626e157ae988c1c39de2daed4898608aaa04
Didier04x <didier.sebas80@gmail.com>
2025-10-04 17:56:39 -0500

    feat(authentication): add authentication services

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,17 @@
 export const environment = {
   production: true,
   apiUrl: 'http://localhost:3000/api/v1',
+  warehousesEndpointPath: '/warehouses',
+  productsEndpointPath: '/products',
+  reportsEndpointPath: '/reports',
+  zonesEndpointPath: '/zones',
+  userEndpointPath: '/users',
+  profileEndpointPath: '/profiles',
+  accountsEndpointPath: '/accounts',
+  catalogEndpointPath: '/catalog',
+  authEndpointPath: '/authentications',
   baseServerUrl: 'http://localhost:8080/api/v1',
+
+  accountWarehousesEndpointPath: '/accounts/{accountId}/warehouses',
+  accountProductsEndpointPath: '/accounts/{accountId}/products',
 };
