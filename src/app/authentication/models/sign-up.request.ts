5291505d21608c97f61ddb8393e1f1f90e9ad8cf
Didier04x <didier.sebas80@gmail.com>
2025-10-04 18:00:26 -0500

    feat(accounts): add authentication services

--- src/app/authentication/models/sign-up.request.ts
+++ src/app/authentication/models/sign-up.request.ts
@@ -1,2 +1,20 @@
 export class SignUpRequest {
+  public username: string;
+  public password: string;
+  public validatePassword: string;
+  public businessName: string;
+  public accountRole: string;
+
+  /**
+   * Constructor.
+   * @param username The username.
+   * @param password The password.
+   */
+  constructor(username: string, password: string, validatePassword: string, businessName: string, accountRole: string) {
+    this.password = password;
+    this.username = username;
+    this.validatePassword = validatePassword;
+    this.businessName = businessName;
+    this.accountRole = accountRole;
+  }
 }
--- src/app/authentication/models/sign-up.response.ts
+++ src/app/authentication/models/sign-up.response.ts
@@ -1,2 +1,9 @@
 export class SignUpResponse {
+  public accountId: number;
+  public username: string;
+
+  constructor(accountId: number, username: string) {
+    this.username = username;
+    this.accountId = accountId;
+  }
 }
