649871b9db8c7d985f30ba792495fd82b51ce3a5
Didier04x <didier.sebas80@gmail.com>
2025-10-04 18:02:11 -0500

    feat(authentication): add confirmation code funcion,section and update confimate code functions

--- src/app/authentication/pages/confirmation-code/confirmation-code.component.ts
+++ src/app/authentication/pages/confirmation-code/confirmation-code.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-confirmation-code',
-  imports: [],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule
+  ],
   templateUrl: './confirmation-code.component.html',
-  styleUrl: './confirmation-code.component.css'
+  styleUrls: ['./confirmation-code.component.css']
 })
 export class ConfirmationCodeComponent {
+  code: string[] = ['', '', '', '', '', ''];
+  codeArray = new Array(6);
 
+  constructor(private router: Router) {}
+
+  autoFocusNext(event: any, index: number) {
+    const input = event.target;
+    let value = input.value.toUpperCase();
+
+    if (!/^[A-Z0-9]$/.test(value)) {
+      input.value = '';
+      this.code[index] = '';
+      return;
+    }
+
+    this.code[index] = value;
+    input.value = value;
+
+    if (index < 5) {
+      input.nextElementSibling?.focus();
+    }
+  }
+
+  goToLogin() {
+    console.log('Code:', this.code);
+    this.router.navigate(['/login']);
+  }
 }
--- src/app/authentication/pages/password-recover/password-recover.component.ts
+++ src/app/authentication/pages/password-recover/password-recover.component.ts
@@ -1,11 +1,45 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-password-recover',
-  imports: [],
   templateUrl: './password-recover.component.html',
-  styleUrl: './password-recover.component.css'
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    CommonModule
+
+  ],
+  styleUrls: ['./password-recover.component.css']
 })
 export class PasswordRecoverComponent {
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder, private router: Router) {
+    this.form = this.fb.group({
+      email: ['', [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.goToConfirmation();
+    }
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+
+  goToConfirmation() {
+    const email = this.form.value.email;
+    console.log('Sending password recovery email to:', email);
+    this.router.navigate(['/confirmation-code']);
+  }
 }
