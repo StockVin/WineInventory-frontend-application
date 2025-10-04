fb6547211bdd8c4ed03f46b8d6035d3955660c8c
Didier04x <didier.sebas80@gmail.com>
2025-10-04 17:59:17 -0500

    feat(accounts): add authentication service

--- src/app/authentication/models/sign-in.request.ts
+++ src/app/authentication/models/sign-in.request.ts
@@ -1,2 +1,14 @@
 export class SignInRequest {
+  public username: string;
+  public password: string;
+
+  /**
+   * Constructor for SignInRequest
+   * @param username The username of the user
+   * @param password The password of the user
+   */
+  constructor(username: string, password: string) {
+    this.username = username;
+    this.password = password;
+  }
 }
