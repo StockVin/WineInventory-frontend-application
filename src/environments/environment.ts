8050c935a8565d5f3253f6e1eef70698d0757263
Didier04x <didier.sebas80@gmail.com>
2025-10-04 17:57:11 -0500

    feat(SAP): add product logic

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,18 @@
 export const environment = {
   production: false,
   apiUrl: 'http://localhost:3000/api/v1',
+  warehousesEndpointPath: '/warehouses',
+  productsEndpointPath: '/products',
+  reportsEndpointPath: '/reports',
+  zonesEndpointPath: '/zones',
+  userEndpointPath: '/users',
+  profileEndpointPath: '/profiles',
+  accountsEndpointPath: '/accounts',
+  catalogEndpointPath: '/catalog',
   baseServerUrl: 'http://localhost:8080/api/v1',
+  authEndpointPath: '/authentications',
+
+  accountWarehousesEndpointPath: '/accounts/{accountId}/warehouses',
+  accountProductsEndpointPath: '/accounts/{accountId}/products',
 };
+
