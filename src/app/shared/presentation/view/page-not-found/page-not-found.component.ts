680ca9fe5db8c1b4f961da2e1febba7528497f40
equinox-1092 <sdiaz1234di@gmail.com>
2025-10-08 15:45:10 -0500

    feat(page-not-found): add rout back

--- src/app/shared/presentation/view/page-not-found/page-not-found.component.ts
+++ src/app/shared/presentation/view/page-not-found/page-not-found.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { Location } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
@@ -8,5 +10,16 @@ import { TranslateModule } from '@ngx-translate/core';
   styleUrl: './page-not-found.component.css'
 })
 export class PageNotFoundComponent {
+  constructor(private router: Router, private location: Location) { }
 
+  ngOnInit(): void {
+    
+  }
+  goBack(): void {
+    if (window.history.length > 1) {
+      this.location.back();
+    } else {
+      this.router.navigate(['/']);
+    }
+  }
 }
