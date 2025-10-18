d3b6edcff302513747fccabff10fede16165d6bb
equinox-1092 <sdiaz1234di@gmail.com>
2025-10-18 16:09:08 -0500

    feat(profile): add the configuration of the endpoint of PUT y GET for profile

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-import {HttpClient, provideHttpClient, withInterceptors} from '@angular/common/http';
-import {TranslateHttpLoader, provideTranslateHttpLoader} from '@ngx-translate/http-loader';
-import {provideTranslateService, TranslateLoader} from '@ngx-translate/core';
+import { HttpClient, provideHttpClient, withInterceptors } from '@angular/common/http';
+import { TranslateHttpLoader, provideTranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideTranslateService } from '@ngx-translate/core';
+import { authenticationInterceptor } from './authentication/services/authentication.interceptor';
 
 export const HttpLoaderFactory = (http: HttpClient) => {
   return new TranslateHttpLoader();
@@ -13,9 +14,8 @@ export const HttpLoaderFactory = (http: HttpClient) => {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
+    provideHttpClient(withInterceptors([authenticationInterceptor])),
     provideRouter(routes),
-    provideHttpClient(),
     provideTranslateService({
       loader: provideTranslateHttpLoader({prefix: './i18n/', suffix: '.json'}),
       fallbackLang: 'en'
